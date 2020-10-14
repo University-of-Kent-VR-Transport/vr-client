606657626c023e883f4312d9a7103df562404d85
HenryBrown0 <me@henrybrown0.com>
2020-10-14 15:58:43 +0100

    Add HelloWorldTest with test config for project

---
+++ Assets/Tests/HelloWorldTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+	public class HelloWorldTest
+	{
+		[Test]
+		public void testAdd()
+		{
+			var helloWorld = new HelloWorld();
+			
+			var output = helloWorld.add(1, 2);
+			Assert.AreEqual(3, output);
+		}
+	}
+}
